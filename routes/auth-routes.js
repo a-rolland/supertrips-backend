0d5f2ad698f52337a723049b75b0a8fdf5b0fffd
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-08-22 11:39:06 +0200

    format documents

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,57 +2,57 @@
 // const passport   = require('passport');
 // const bcrypt     = require('bcryptjs');
 // const User       = require('../models/user-model');
-// const authRoutes = express.Router(); 
- 
+// const authRoutes = express.Router();
+
 // authRoutes.post('/signup', (req, res, next) => {
 //     const username = req.body.username;
 //     const password = req.body.password;
-  
+
 //     if (!username || !password) {
 //       res.status(400).json({ message: 'Provide username and password' });
 //       return;
 //     }
- 
+
 //     if(password.length < 7){
 //         res.status(400).json({ message: 'Please make your password at least 8 characters long for security purposes.' });
 //         return;
 //     }
-  
+
 //     User.findOne({ username }, (err, foundUser) => {
- 
+
 //         if(err){
 //             res.status(500).json({message: "Username check went bad."});
 //             return;
 //         }
- 
+
 //         if (foundUser) {
 //             res.status(400).json({ message: 'Username taken. Choose another one.' });
 //             return;
 //         }
-  
+
 //         const salt     = bcrypt.genSaltSync(10);
 //         const hashPass = bcrypt.hashSync(password, salt);
-  
+
 //         const aNewUser = new User({
 //             username:username,
 //             password: hashPass
 //         });
-  
+
 //         aNewUser.save(err => {
 //             if (err) {
 //                 res.status(400).json({ message: 'Saving user to database went wrong.' });
 //                 return;
 //             }
-            
+
 //             // Automatically log in user after sign up
 //             // .login() here is actually predefined passport method
 //             req.login(aNewUser, (err) => {
- 
+
 //                 if (err) {
 //                     res.status(500).json({ message: 'Login after signup went bad.' });
 //                     return;
 //                 }
-            
+
 //                 // Send the user's information to the frontend
 //                 // We can use also: res.status(200).json(req.user);
 //                 res.status(200).json(aNewUser);
@@ -60,14 +60,14 @@
 //         });
 //     });
 // });
- 
+
 // authRoutes.post('/login', (req, res, next) => {
 //   passport.authenticate('local', (err, theUser, failureDetails) => {
 //       if (err) {
 //           res.status(500).json({ message: 'Something went wrong authenticating user' });
 //           return;
 //       }
-  
+
 //       if (!theUser) {
 //           // "failureDetails" contains the error messages
 //           // from our logic in "LocalStrategy" { message: '...' }.
@@ -94,7 +94,6 @@
 //   res.status(200).json({ message: 'Log out success!' });
 // });
 
-
 // authRoutes.get('/loggedin', (req, res, next) => {
 //   // req.isAuthenticated() is defined by passport
 //   if (req.isAuthenticated()) {
@@ -106,21 +105,20 @@
 
 // module.exports = authRoutes;
 
-
 const express = require("express");
 const router = express.Router();
 
 const {
-    postSignup,
-    postLogin,
-    postLogout,
-    getLoggedIn
+  postSignup,
+  postLogin,
+  postLogout,
+  getLoggedIn,
 } = require("../controllers/auth-controller");
 
 router
   .post("/signup", postSignup)
   .post("/login", postLogin)
   .post("/logout", postLogout)
-  .get("/loggedIn", getLoggedIn)
+  .get("/loggedIn", getLoggedIn);
 
 module.exports = router;
