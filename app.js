67a49aa61c439e2b28cd3c6670a451f5229b039f
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-09-11 17:03:23 +0200

    test deploy 3

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/client/build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Express View engine setup
 
