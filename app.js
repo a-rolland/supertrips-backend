9d7d6c9fd83d8a1dfb8a4308b0023b7155745a80
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-09-15 20:12:37 +0200

    idem

--- app.js
+++ app.js
@@ -42,8 +42,10 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
+// app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 
 // Session configs - first version
 // app.use(
---
+++ build/precache-manifest.aedaa28bf99ac7059d04dbb4f5f30e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131921d60263d20182065967377240a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e331dd10b940c8055b2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82a32fc1cf0d1423b643",
+    "url": "/static/js/2.39fb19db.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/static/js/2.39fb19db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e331dd10b940c8055b2",
+    "url": "/static/js/main.5574e743.chunk.js"
+  },
+  {
+    "revision": "c24f8aa335f35f8d2992",
+    "url": "/static/js/runtime-main.6aec1922.js"
+  }
+]);
